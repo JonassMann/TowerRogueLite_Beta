195b118ab6fc9741aeeeaab2ceb52d9964f9200a
Jonas Ingelstad <JonasIngelstad@Gmail.com>
2022-05-07 09:38:32 +0200

    Add movement restrictors

--- Assets/Scripts/StateSystem/StateEffect.cs
+++ Assets/Scripts/StateSystem/StateEffect.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public abstract class StateEffect : MonoBehaviour
 {
-    public StatBlock statBlock;
     public abstract void OnStart(GameObject user, GameObject target, GameObject moveTarget);
     public abstract State OnUpdate(GameObject user, GameObject target, GameObject moveTarget);
     public abstract void OnEnd(GameObject user, GameObject target, GameObject moveTarget);
