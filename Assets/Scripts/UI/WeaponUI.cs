96fd5a66928b5155431a758ec54668da677ceeb7
Jonas Ingelstad <JonasIngelstad@Gmail.com>
2022-05-19 22:42:15 +0200

    Auto stash before merge of "main" and "origin/main"

--- Assets/Scripts/UI/WeaponUI.cs
+++ Assets/Scripts/UI/WeaponUI.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WeaponUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Character player;
+
+    public Image selectedWeapon;
+
+    public List<Sprite> selectedSprites;
+
+    public Image leftWeapon;
+    public Image rightWeapon;
+
+    private int selected;
+
+    private void Awake()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Character>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        selected = player.activeWeapon;
+    }
+
+    private void Update()
+    {
+        if (player.activeWeapon != selected)
+        {
+            selected = player.activeWeapon;
+            selectedWeapon.sprite = selectedSprites[selected - 1];
+        }
+
+        leftWeapon.sprite = player.weapons.Count > 0 ? player.weapons[0].sprite : null;
+        rightWeapon.sprite = player.weapons.Count > 1 ? player.weapons[1].sprite : null;
     }
 }
