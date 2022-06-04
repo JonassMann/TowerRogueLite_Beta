355998d3230db9054b384269526f64b22c3362d7
Jonas Ingelstad <JonasIngelstad@Gmail.com>
2022-06-04 21:21:38 +0200

    Possible load fix

--- Assets/_Testing/Kristina/Scripts/LoadRun.cs
+++ Assets/_Testing/Kristina/Scripts/LoadRun.cs
@@ -10,7 +10,7 @@ public class LoadRun : MonoBehaviour
 
    private void OnTriggerEnter2D (Collider2D other)
     {
-        Invoke("StartingRoom", delayedtime);
+        Invoke(nameof(GameScene), delayedtime);
     }
 
     public void GameScene()
