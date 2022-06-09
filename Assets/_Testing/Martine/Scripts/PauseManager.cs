dbb288078b2bef2df1c460d529b06402e82add82
Jonas Ingelstad <JonasIngelstad@Gmail.com>
2022-06-09 12:00:02 +0200

    Changes

--- Assets/_Testing/Martine/Scripts/PauseManager.cs
+++ Assets/_Testing/Martine/Scripts/PauseManager.cs
@@ -33,7 +33,7 @@ public class PauseManager : MonoBehaviour
 
 
     public void exit() {
-        SceneManager.LoadScene("MainMenu", LoadSceneMode.Additive);
+        SceneManager.LoadScene(0);
     }
 
     
